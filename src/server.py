5c531ee9f941db25a450488e955d1100e75327a1
py.Travis <11712610@mail.sustech.edu.cn>
2020-10-22 10:29:49 +0800

    1029

--- src/server.py
+++ src/server.py
@@ -1,12 +1,13 @@
 
 
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding:utf-8 -*-
 import sys
 
 from src.Task import DIStoOBSscheduleTask
 
-#5分钟一次
+# 5分钟一次
+
 disToObsSchedule = DIStoOBSscheduleTask(10)
 disToObsSchedule.run(1)
 
